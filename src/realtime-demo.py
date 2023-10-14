88f2e7ee97d62ca8040e30dd8e43da706d00cd6e
Ruben Nuñez <ruben.nunez@live.de>
2023-10-15 00:34:16 +0200

    fix ifft example

--- src/realtime-demo.py
+++ src/realtime-demo.py
@@ -1,33 +1,45 @@
-"""PyAudio Example: Record a few seconds of audio and save to a wave file."""
-
-import wave
 import sys
+import wave
+import sounddevice as sd
+import numpy as np
 
-import pyaudio
-
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1 if sys.platform == 'darwin' else 2
-RATE = 44100
-RECORD_SECONDS = 5
-
-#p = pyaudio.PyAudio()
-#for i in range(p.get_device_count()):
-#    info = p.get_device_info_by_index(i)
-#    print(info['index'], info['name'])
-
-with wave.open('output.wav', 'wb') as wf:
-    p = pyaudio.PyAudio()
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, input_device_index=5)
+def record_audio_to_wav_file(filename, record_seconds, samplerate=44100, channels=2, device=0):
     
+    if sys.platform == 'darwin':
+        channels = 1
+
     print('Recording...')
-    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
-        wf.writeframes(stream.read(CHUNK))
-    print('Done')
+    recording = sd.rec(int(samplerate * record_seconds), 
+                       samplerate=samplerate, 
+                       channels=channels, 
+                       device=device,
+                       dtype='int16',)
+    sd.wait()  
+    print('Recording completed.')
+
+    with wave.open(filename, 'wb') as wf:
+        wf.setnchannels(channels)
+        wf.setsampwidth(2)  # 2 bytes for 'int16'
+        wf.setframerate(samplerate)
+        wf.writeframes(recording.tobytes())
+
+if __name__ == '__main__':
     
-    stream.close()
-    p.terminate()
+    default_input_index = sd.default.device[0]
+    default_input_name = sd.query_devices()[default_input_index]['name']
+
+    print(f'Default input device index: {default_input_index}, Name: {default_input_name}')
+    
+    print('\n')
+
+    print('Available input devices:')
+
+    devices = sd.query_devices()
+    for idx, device in enumerate(devices):
+        print(idx, device['name'])
+    
+    print('\n')
+    print('Please select the input device index:')
+
+    device_idx = int(input())
+    record_audio_to_wav_file('output.wav', 10, device=device_idx)
