e3444323119ad0c58aabe4e48ff6b44ac3ba6bbe
Ruben Nuñez <ruben.nunez@live.de>
2023-10-22 22:28:12 +0200

    writing chapter "Stand der Forschung"

--- code-wav2vec2/preview.py
+++ code-wav2vec2/preview.py
@@ -4,20 +4,25 @@ import soundfile as sf
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 import torch
 
-# Record audio from the microphone
 def record_audio(duration, samplerate=16000):
+    """Record audio from the microphone."""
     print("Recording...")
-    myrecording = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=2, dtype='float64')
+    myrecording = sd.rec(
+        int(samplerate * duration), 
+        samplerate=samplerate, 
+        channels=1, 
+        dtype='float64'
+    )
     sd.wait()  # Wait until recording is finished
     print("Recording finished.")
     return myrecording
 
-# Save the recording as a wav file
 def save_wav(filename, recording, samplerate=16000):
+    """Save the recording as a wav file."""
     sf.write(filename, recording, samplerate)
 
-# Main transcription function
 def transcribe(filename):
+    """Main transcription function."""
     # Load model and tokenizer
     processor = Wav2Vec2Processor.from_pretrained("./wav2vec2-base-960h")
     model = Wav2Vec2ForCTC.from_pretrained("./wav2vec2-base-960h")
@@ -25,12 +30,17 @@ def transcribe(filename):
     # Load the recorded audio file
     input_audio, _ = sf.read(filename, dtype="float32")
     
-    # Make sure audio is mono (since we recorded with 2 channels)
+    # Ensure audio is mono (since we recorded with 2 channels)
     if len(input_audio.shape) > 1:
         input_audio = np.mean(input_audio, axis=1)
     
     # Tokenize
-    input_values = processor(input_audio, return_tensors="pt", padding="longest").input_values
+    input_values = processor(
+        input_audio, 
+        return_tensors="pt", 
+        padding="longest", 
+        sampling_rate=16000
+    ).input_values
     
     # Retrieve logits
     logits = model(input_values).logits
@@ -42,8 +52,7 @@ def transcribe(filename):
     return transcription
 
 if __name__ == "__main__":
-    recording = record_audio(10)  # Record for 10 seconds
+    recording = record_audio(5)  # Record for 5 seconds
     save_wav("recorded_audio.wav", recording)
     result = transcribe("recorded_audio.wav")
     print("Transcription:", result[0])
-
